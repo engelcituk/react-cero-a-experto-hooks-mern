c3ce532e59b0b4efcdf5ecc1a3a82848ec2b0421
cituk <programaciondyn3.mx@sandos.com>
2020-07-01 02:22:24 -0500

    Variables y constantes

---
+++ 02-intro-javascript/src/bases/01-const-let.js
@@ -0,0 +1,15 @@
+// Variables y constantes // ya no se debe usar var
+const nombre = 'Luis';
+let apellido = 'Perez';
+
+let valorDado=5;
+
+console.log(nombre, apellido, valorDado);
+
+if(true){
+    const nombre = 'Luis';
+    let valorDado= 8;
+    console.log(nombre,valorDado);
+}
+
+console.log(nombre, apellido, valorDado);
--- 02-intro-javascript/src/index.js
+++ 02-intro-javascript/src/index.js
@@ -1,1 +1,3 @@
-console.log('hola mundo')
+
+
+console.log('apellido');

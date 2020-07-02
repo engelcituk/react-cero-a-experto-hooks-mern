7473144577ca7fb4195a879577455fd5b69ed973
cituk <programaciondyn3.mx@sandos.com>
2020-07-02 01:27:55 -0500

    fin

--- 02-intro-javascript/src/index.js
+++ 02-intro-javascript/src/index.js
@@ -1,9 +1,1 @@
-const activo = true;
-
-const mensaje = (activo) ? 'Activo' : 'Inactivo';
-
-console.log(mensaje)
-
-const mensaje2 = activo && 'ACtivo'; //un if mas simplificado
-
-console.log(mensaje2)
+git s

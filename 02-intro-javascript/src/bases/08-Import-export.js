ddb85bc875fe9a77005e17999f59cacd2534ec9c
cituk <programaciondyn3.mx@sandos.com>
2020-07-01 22:50:12 -0500

    import, exports, multimples maneras de hacerlo

---
+++ 02-intro-javascript/src/bases/08-Import-export.js
@@ -0,0 +1,20 @@
+//import {heroes} from './data/heroes'; // se puede importar con imp
+
+/*no hace falta ponerle llaves a heroes porque el array se exporta por default, al importar le damos el nombre que queramos*/
+import heroes, {owners} from './data/heroes'; 
+
+//import {heroes, owners} from './data/heroes'; 
+
+
+const getHeroeById = (id)=> {
+    return heroes.find( (heroe) => heroe.id === id)
+}
+
+console.log(getHeroeById(2));//encuentra el primero que cumple la condición
+
+const getHeroeByOwner= (owner)=> {
+    return heroes.filter( (heroe) => heroe.owner === owner)
+}
+
+console.log(getHeroeByOwner('DC'));//retorna los objetos que cumplen la condicion
+console.log(owners);//retorna los objetos que cumplen la condicion

f654f46cb9fdac7b5bbb49300c91d9b659e941dc
cituk <programaciondyn3.mx@sandos.com>
2020-07-05 22:25:53 -0500

    Comunicación entre componentes - Props

--- 03-counter-app/src/PrimeraApp.js
+++ 03-counter-app/src/PrimeraApp.js
@@ -1,17 +1,19 @@
 import React, { Fragment } from 'react';
 
 //Funcional components
-const PrimeraApp = () => {
+const PrimeraApp = ({saludar}) => {
     const saludo =  'Hola mundo desde una constante';
     const objeto = {
         nombre: 'Juan',
         edad: 28
     }
+    
     return (
         <> {/* los fragment se pueden poner omitiendo el texto Fragment*/}
             <h1>{saludo}</h1>
             <pre>objeto {JSON.stringify(objeto, null, 3)}</pre>
             <p>Mi primera aplicación</p>
+            <p>props {saludar}</p>
         </>
     );
 }

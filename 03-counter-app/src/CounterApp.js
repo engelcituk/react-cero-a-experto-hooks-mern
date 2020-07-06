0697ad710015e0772c57e6a3829c8c15b92ec828
cituk <programaciondyn3.mx@sandos.com>
2020-07-06 00:42:00 -0500

    useState - Hook

--- 03-counter-app/src/CounterApp.js
+++ 03-counter-app/src/CounterApp.js
@@ -1,18 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types'
 
 //Funcional components
 const CounterApp = ({value}) => {
 
+    const [counter, setCounter] = useState(0); //retorna un arreglo []
+
+
     //handleAdd
     const handleAdd = (e) => {
-        console.log(e);
+       setCounter( counter + 1);
+        // setCounter( (c) => c + 1)
+
     }
 
     return (
         <> 
         <h1>CounterApp</h1>
-        <h2> { value } </h2>
+        <h2> { counter } </h2>
 
         {/* <button onClick={ (e) => {handleAdd(e)} } >+1</button> */}
          <button onClick={ (e) => {handleAdd(e)} } >+1</button> {/*simplificado */}

65cfe036220de04ac8274027a268c64185b9b7f6
cituk <programaciondyn3.mx@sandos.com>
2020-07-05 19:47:56 -0500

    Nuestro primer Componente

---
+++ 03-counter-app/src/PrimeraApp.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+//Funcional components
+const PrimeraApp = () => {
+    return <h1>Hola mundo</h1>;
+}
+
+export default PrimeraApp;
--- 03-counter-app/src/index.js
+++ 03-counter-app/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PrimeraApp from './PrimeraApp';
+import './index.css'
 
-const saludo = <h1>Hola mundo</h1>;
 
 const divRoot = document.querySelector('#root');
 
-ReactDOM.render(saludo,divRoot);
+ReactDOM.render(<PrimeraApp />,divRoot);

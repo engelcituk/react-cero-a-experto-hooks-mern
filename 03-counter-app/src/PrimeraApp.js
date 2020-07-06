366ac7fd716dee3cbfa73f3bb67f3846cbf2c06d
cituk <programaciondyn3.mx@sandos.com>
2020-07-05 23:22:44 -0500

    DefaultProps

--- 03-counter-app/src/PrimeraApp.js
+++ 03-counter-app/src/PrimeraApp.js
@@ -1,8 +1,8 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types'
 
 //Funcional components
-const PrimeraApp = ({saludar}) => {
+const PrimeraApp = ({saludar, subtitulo}) => {
     const saludo =  'Hola mundo desde una constante';
     const objeto = {
         nombre: 'Juan',
@@ -12,6 +12,7 @@ const PrimeraApp = ({saludar}) => {
     return (
         <> {/* los fragment se pueden poner omitiendo el texto Fragment*/}
             <h1>{saludo}</h1>
+            <h3>{subtitulo}</h3>
             <pre>objeto {JSON.stringify(objeto, null, 3)}</pre>
             <p>Mi primera aplicación</p>
             <p>props {saludar}</p>
@@ -22,4 +23,9 @@ const PrimeraApp = ({saludar}) => {
 PrimeraApp.propType = {
     saludar: PropTypes.string.isRequired
 }
+
+PrimeraApp.defaultProps = {
+    subtitulo: 'soy el subtitulo'
+}
+
 export default PrimeraApp;

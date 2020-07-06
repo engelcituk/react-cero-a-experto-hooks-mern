c714e43a918d1a26c75e16da03f593d9ec69573a
cituk <programaciondyn3.mx@sandos.com>
2020-07-06 00:21:46 -0500

    Evento click (Eventos en general)

--- 03-counter-app/src/CounterApp.js
+++ 03-counter-app/src/CounterApp.js
@@ -3,11 +3,20 @@ import PropTypes from 'prop-types'
 
 //Funcional components
 const CounterApp = ({value}) => {
- 
+
+    //handleAdd
+    const handleAdd = (e) => {
+        console.log(e);
+    }
+
     return (
         <> 
         <h1>CounterApp</h1>
         <h2> { value } </h2>
+
+        {/* <button onClick={ (e) => {handleAdd(e)} } >+1</button> */}
+         <button onClick={ (e) => {handleAdd(e)} } >+1</button> {/*simplificado */}
+
         </>
     );
 }

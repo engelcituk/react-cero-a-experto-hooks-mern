d816ce8bff74d0291ac0c8cfc67b7c412a3816c7
cituk <programaciondyn3.mx@sandos.com>
2020-07-10 02:56:30 -0500

    Pruebas con el botón de reset

--- 03-counter-app/src/tests/CounterApp.test.js
+++ 03-counter-app/src/tests/CounterApp.test.js
@@ -23,14 +23,32 @@ describe('Pruebas en <CounterApp/>', ()=> {
     });
 
     test('Debe de incrementar con el botón +1', () => {
+
          wrapper.find('button').at(0).simulate('click'); 
         const counterText = wrapper.find('h2').text().trim();
         expect(counterText).toBe('11');
+
     });
+
     test('Debe de restar con el botón 1', () => {
+
         wrapper.find('button').at(2).simulate('click'); 
        const counterText = wrapper.find('h2').text().trim();
        expect(counterText).toBe('9');
+
    });
+
+   test('debe de colocar el valor por defecto con el botón reset ', () => {
+        const wrapper = shallow( <CounterApp value={105}/> );
+        wrapper.find('button').at(0).simulate('click'); 
+        wrapper.find('button').at(0).simulate('click'); 
+        wrapper.find('button').at(1).simulate('click'); 
+
+        const counterText = wrapper.find('h2').text().trim();
+
+        expect(counterText).toBe('105');
+       
+   })
+   
      
 })

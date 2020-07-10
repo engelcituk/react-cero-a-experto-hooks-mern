c20caba71565f35990ed6f1a98df49391ddbc073
cituk <programaciondyn3.mx@sandos.com>
2020-07-09 20:37:57 -0500

    toEqual

--- 03-counter-app/src/base/05-funciones.js
+++ 03-counter-app/src/base/05-funciones.js
@@ -11,30 +11,20 @@ const saludar2 = ( nombre ) => {
 const saludar3 = ( nombre ) => `Hola, ${ nombre }`;
 const saludar4 = () => `Hola Mundo`;
 
-// console.log( saludar('Goku') )
 
-console.log( saludar2('Vegeta') );
-console.log( saludar3('Goku') );
-console.log( saludar4() );
-
-
-const getUser = () => ({
+export const getUser = () => ({
         uid: 'ABC123',
         username: 'El_Papi1502'
 });
 
 
-const user = getUser();
-console.log(user);
-
 // Tarea
-const getUsuarioActivo = ( nombre ) =>({
+export const getUsuarioActivo = ( nombre ) =>({
     uid: 'ABC567',
     username: nombre
 })
 
-const usuarioActivo = getUsuarioActivo('Fernando');
-console.log( usuarioActivo );
+
 
 
 
---
+++ 03-counter-app/src/tests/base/05-funciones.test.js
@@ -0,0 +1,29 @@
+import '@testing-library/jest-dom';
+import { getUser, getUsuarioActivo } from '../../base/05-funciones';
+
+describe('Pruebas en el archivo 02-template-string.js', ()=> {
+    test('getUser Debe de retornar un objeto', () => {
+        const userTest = {
+            uid: 'ABC123',
+            username: 'El_Papi1502'
+        }
+
+        const user = getUser();
+
+        expect(user).toEqual(userTest)
+    })
+    
+    test('getUsuarioActivo Debe de retornar un objeto', () => {
+        const nombre = 'Pedro';
+
+        const userTest = {
+            uid: 'ABC567',
+            username: nombre
+        }
+
+        const user = getUsuarioActivo(nombre);
+
+        expect(user).toEqual(userTest)
+    })
+})
+

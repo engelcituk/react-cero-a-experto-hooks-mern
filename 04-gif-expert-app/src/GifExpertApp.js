ee2d2ab4bea39cf2df030e3629f95887e655d100
cituk <programaciondyn3.mx@sandos.com>
2020-07-10 13:25:31 -0500

    GifExpertApp - Component

---
+++ 04-gif-expert-app/src/GifExpertApp.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+//Funcional components
+const GifExpertApp = () => {
+    return (
+        <> 
+            <h1>GifExpertApp</h1>
+            <hr/>
+        </>
+    );
+}
+
+export default GifExpertApp;

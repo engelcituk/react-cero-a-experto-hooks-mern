434fae1e43d649f3dda6a655de05adbf0cdbb4f3
cituk <programaciondyn3.mx@sandos.com>
2020-07-09 19:55:33 -0500

    Jest - Expect - toBe

--- 03-counter-app/src/tests/demo.test.js
+++ 03-counter-app/src/tests/demo.test.js
@@ -1,6 +1,14 @@
-test('debe de ser true', () => {
-    const isActive = true;
-    if(isActive){
-        throw new Error('No está activo');
-    }
+describe('Pruebas en el archivo demo.test.js', ()=> {
+    test('deben de ser iguales los string', () => {
+        //1.Inicializacion
+         const mensaje = 'Hola mundo';
+     
+         //2. Estimulo
+         const mensaje2 = `Hola mundo`;
+     
+         //3. Observa el comportamiento
+         expect(mensaje).toBe(mensaje2); // 
+     
+     })
 })
+

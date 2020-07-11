b1eefee67ed58c101a1f0c567d815985b4ea2227
cituk <programaciondyn3.mx@sandos.com>
2020-07-10 20:27:31 -0500

    Creando una lista de categorias

--- 04-gif-expert-app/src/GifExpertApp.js
+++ 04-gif-expert-app/src/GifExpertApp.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 //Funcional components
 const GifExpertApp = () => {
+    //const categories = ['One Punch', 'Dragon Ball','Doctor Stone', 'One Piece'];
+    const [categories, setCategories] = useState(['One Punch', 'Dragon Ball','Doctor Stone', 'One Piece']);
+
+ 
+    const handleAdd = (e) => {
+        //spread operador, se obtiene todas las categories y luego se añade el otro elemento al estado (arreglo)
+        setCategories( [...categories, 'Shingeky']);
+        setCategories( cats => [...cats, 'Shingeky']); // toma el estado original, lo cambie y regresa el nuevo estado
+     }
     return (
         <> 
             <h1>GifExpertApp</h1>
             <hr/>
+            <button onClick={ handleAdd }>Agregar</button>
+            <ol>
+                {
+                    categories.map( category => {
+                        return <li key={category}> {category}</li>
+                    })
+                }
+            </ol>
         </>
     );
 }
 
-export default GifExpertApp;
+export default GifExpertApp; 
--- 04-gif-expert-app/src/index.js
+++ 04-gif-expert-app/src/index.js
@@ -9,5 +9,3 @@ ReactDOM.render(
     <GifExpertApp />,
   document.getElementById('root')
 );
-
-

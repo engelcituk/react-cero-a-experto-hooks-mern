08b8c6501d76b7101abe7bd4df6c9908806938bf
cituk <programaciondyn3.mx@sandos.com>
2020-07-12 01:07:18 -0500

    docs agregado, para github pages

---
+++ 04-gif-expert-app/docs/precache-manifest.4808a211dd6a57d9aa87c64932b05dd4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "961289042576bbf11953f4ba97d67ac3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "caf5d66315d4376e0d6a",
+    "url": "/static/css/main.7284a950.chunk.css"
+  },
+  {
+    "revision": "c74871274a30e1ed47df",
+    "url": "/static/js/2.702a4b08.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.702a4b08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "caf5d66315d4376e0d6a",
+    "url": "/static/js/main.f36a8c93.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);

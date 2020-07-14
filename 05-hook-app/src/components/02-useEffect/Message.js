4a0f05ff4fa20eccee89dcd5d2751c35eb7f2ee4
cituk <programaciondyn3.mx@sandos.com>
2020-07-14 16:40:06 -0500

    useEffect - Precauciones

--- 05-hook-app/src/components/02-useEffect/Message.js
+++ 05-hook-app/src/components/02-useEffect/Message.js
@@ -1,15 +1,28 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const Message = () => {
+    const [coords, setCoords] = useState({x:0, y:0}); //estado del componente // coordenadas , valor inicial un objeto en ceros
+    const {x,y}= coords;
+
     useEffect(() => {
-        console.log('componente montado');
+        //lo que sucede cuando se monta el componente
+        const mouseMove = (e) => {
+            const coords = {x: e.x, y: e.y};
+            setCoords(coords)
+        }
+        window.addEventListener('mousemove', mouseMove);
         return () => {
-        console.log('componente desmontado');
+            //se desmonta el componente
+            window.removeEventListener('mousemove', mouseMove);
         }
     }, [])
     return (
         <div>
             <h3>Mensaje</h3>
+            <p>
+                cooordenadas:
+                x:{x} y:{y}
+            </p>
         </div>
     )
 }

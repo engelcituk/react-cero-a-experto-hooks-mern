6a7c423d79f869bf8c71184f7b94801712b11d8a
cituk <programaciondyn3.mx@sandos.com>
2020-07-13 22:33:18 -0500

    Inicio de proyecto - Hooks

---
+++ 05-hook-app/src/HooksApp.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const HooksApp = () => {
+    return (
+        <div>
+            <h1>HooksApp</h1>
+        </div>
+    )
+}
---
+++ 05-hook-app/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { HooksApp } from './HooksApp';
+
+
+ReactDOM.render(
+ 
+    <HooksApp />,
+  document.getElementById('root')
+);
+

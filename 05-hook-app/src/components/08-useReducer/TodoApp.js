f9ae105f8e1565871cb3e8b2e00d09538eaca8eb
cituk <programaciondyn3.mx@sandos.com>
2020-07-25 18:30:24 -0500

    Creando el cascaron de la lista de TODOs

--- 05-hook-app/src/components/08-useReducer/TodoApp.js
+++ 05-hook-app/src/components/08-useReducer/TodoApp.js
@@ -5,7 +5,7 @@ import './styles.css';
 
 const initialState = [{
     id : new Date().getTime(),
-    decription:'Aprender React',
+    description:'Aprender React',
     done: false
 }];
 export const TodoApp = () => {
@@ -14,14 +14,44 @@ export const TodoApp = () => {
     console.log(todos);
     return (
         <div>
-            <h1>todoApp</h1>
+            <h1>todoApp {todos.length}</h1>
             <hr/>
-            <ul>
-                <li>Hola</li>
-                <li>Hola mundo</li>
-                <li>Hola de nuevo</li>
+            <div className="row">
+                <div className="col-md-7">
+                    <ul className="list-group list-group-flush">
+                        {
+                            todos.map( (todo,indice) => (
+                                <li
+                                    key={todo.id}
+                                    className='list-group-item'
+                                >
+                                <p className="text-center ">{indice +1} {todo.description}</p>
+                                <button
+                                    className="btn btn-danger"
+                                >Borrar</button>
+                                </li>
+                            ))
+                        }
 
-            </ul>
+                    </ul>
+                </div>
+                <div className="col-md-5">
+                    <h4>Agregar tarea</h4>
+                    <hr/>
+                    <form>
+                        <input
+                            className="form-control"
+                            type="text"
+                            name="description"
+                            placeholder="Indique la tarea a realizar"
+                            autoComplete="off"
+                        />
+                        <button
+                            className="btn btn-primary mt-1 btn-block"
+                        >Agregar</button>
+                    </form>
+                </div>
+            </div>
         </div>
     )
 }

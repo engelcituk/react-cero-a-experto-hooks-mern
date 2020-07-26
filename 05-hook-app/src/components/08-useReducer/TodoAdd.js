d48893cf3b309107976ebac0d1cd9cba23ab1d86
cituk <programaciondyn3.mx@sandos.com>
2020-07-25 21:24:52 -0500

    Optimización #2 - Agregar TODO

---
+++ 05-hook-app/src/components/08-useReducer/TodoAdd.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import { useForm } from '../../hooks/useForm';
+
+export const TodoAdd = ({ handleAddTodo }) => {
+    const [{description}, handleInputChange, reset] = useForm({ //custom hook para manipular campos de formulario
+        description:''
+    });
+
+    const hanleSubmit = (e) => {
+        e.preventDefault();
+
+        if(description.trim().length <= 1){ //si el campo descripcion es menor o igual a 1, se limpia espacios con trim
+            return;
+        }
+        const newTodo = {
+            id : new Date().getTime(),
+            description,
+            done: false
+        }
+
+        handleAddTodo(newTodo); //llama al dispatch
+        reset();//reseteo los campos del formulario
+    }
+    
+    return (
+        <>
+            <h4>Agregar tarea</h4>
+            <hr/>
+            <form
+                onSubmit={hanleSubmit}
+                >
+                <input
+                    className="form-control"
+                    type="text"
+                    name="description"
+                    placeholder="Indique la tarea a realizar"
+                    autoComplete="off"
+                    value={description}
+                    onChange={handleInputChange}
+                />
+                <button
+                    type="submit"
+                    className="btn btn-primary mt-1 btn-block"
+                >Agregar</button>
+            </form>
+        </>
+    )
+}
--- 05-hook-app/src/components/08-useReducer/TodoApp.js
+++ 05-hook-app/src/components/08-useReducer/TodoApp.js
@@ -1,9 +1,9 @@
 import React, { useReducer, useEffect } from 'react'
 import { todoReducer } from './todoReducer';
-import { useForm } from '../../hooks/useForm';
 
 import './styles.css';
 import { TodoList } from './TodoList';
+import { TodoAdd } from './TodoAdd';
 
 
 const init = ( ) => {
@@ -19,9 +19,7 @@ export const TodoApp = () => {
 
     const [todos, dispatch] = useReducer(todoReducer, [], init);
     
-    const [{description}, handleInputChange, reset] = useForm({ //custom hook para manipular campos de formulario
-        description:''
-    });
+    
     useEffect(() => {
         localStorage.setItem('todos', JSON.stringify(todos))
     }, [todos]);
@@ -36,32 +34,19 @@ export const TodoApp = () => {
 
     }
     const handleToggle = (todoId) => {
-        
         dispatch({
             type: 'toggle',
             payload: todoId
         });
     }
-    const hanleSubmit = (e) => {
-        e.preventDefault();
 
-        if(description.trim().length <= 1){ //si el campo descripcion es menor o igual a 1, se limpia espacios con trim
-            return;
-        }
-        const newTodo = {
-            id : new Date().getTime(),
-            description,
-            done: false
-        }
-
-        const action = {
+    const handleAddTodo = (newTodo) => {
+        dispatch({
             type: 'add',
             payload : newTodo
-        }
-        dispatch(action);
-        reset();//reseteo los campos del formulario
-
+        });
     }
+
     return (
         <div>
             <h1>todoApp {todos.length}</h1>
@@ -75,25 +60,9 @@ export const TodoApp = () => {
                     />
                 </div>
                 <div className="col-md-5">
-                    <h4>Agregar tarea</h4>
-                    <hr/>
-                    <form
-                        onSubmit={hanleSubmit}
-                        >
-                        <input
-                            className="form-control"
-                            type="text"
-                            name="description"
-                            placeholder="Indique la tarea a realizar"
-                            autoComplete="off"
-                            value={description}
-                            onChange={handleInputChange}
-                        />
-                        <button
-                            type="submit"
-                            className="btn btn-primary mt-1 btn-block"
-                        >Agregar</button>
-                    </form>
+                    <TodoAdd
+                        handleAddTodo={handleAddTodo}
+                    />
                 </div>
             </div>
         </div>

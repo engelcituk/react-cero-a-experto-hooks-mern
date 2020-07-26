97a0ef5ba8a54fb52f350e7759928486db6329e9
cituk <programaciondyn3.mx@sandos.com>
2020-07-26 01:52:28 -0500

    CreateContext y useContext

--- 05-hook-app/src/components/09-useContext/HomeScreen.js
+++ 05-hook-app/src/components/09-useContext/HomeScreen.js
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import { UserContext } from './UserContext';
 
 export const HomeScreen = () => {
+    const userContext = useContext(UserContext);
+
+    console.log(userContext);
     return (
         <div>
             <h1>HomeScreen</h1>
--- 05-hook-app/src/components/09-useContext/MainApp.js
+++ 05-hook-app/src/components/09-useContext/MainApp.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import { AppRouter } from './AppRouter';
+import { UserContext } from './UserContext';
 
 export const MainApp = () => {
+    const user = {
+        id: 1234,
+        name:'Fercho',
+        email: 'kasdkasd@mail.com'
+    }
     return (
-        <AppRouter/>
+        <UserContext.Provider value={user}>
+            <AppRouter/>
+        </UserContext.Provider>
     )
 }

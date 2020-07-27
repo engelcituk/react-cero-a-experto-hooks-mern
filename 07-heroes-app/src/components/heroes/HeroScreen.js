84f51985dceb6a9995fd0a69bea5118378367cfa
cituk <programaciondyn3.mx@sandos.com>
2020-07-27 18:56:56 -0500

    Leer argumentos por URL

--- 07-heroes-app/src/components/heroes/HeroScreen.js
+++ 07-heroes-app/src/components/heroes/HeroScreen.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { Switch, Route, useParams, Redirect} from 'react-router-dom';
+import { getHeroById } from '../../selectors/getHeroeById';
+
 
 export const HeroScreen = () => {
+    
+    const {heroeId } = useParams();
+
+    const hero = getHeroById(heroeId);
+    // si heroe no existe
+    if(!hero){
+        return <Redirect to="/" />; //redirijo al home
+    }
+    const { superhero, publisher, alter_ego, first_appearance, characters} = hero;
+
     return (
         <div>
-            <h1>HeroScreen</h1>
+            <h1>Hero Screen</h1>
         </div>
     )
 }
--- 07-heroes-app/src/routers/DashboardRoutes.js
+++ 07-heroes-app/src/routers/DashboardRoutes.js
@@ -13,7 +13,7 @@ export const DashboardRoutes = () => {
             <div className="container mt-2">
                 <Switch>
                     <Route exact path="/marvel" component={ MarvelScreen }/>
-                    <Route exact path="/heroe/:heroeId" component={ HeroScreen }/>
+                    <Route exact path="/hero/:heroeId" component={ HeroScreen }/>
                     <Route exact path="/dc" component={ DcScreen }/>
                     <Redirect to="/marvel" />
 

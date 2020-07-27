82c0ff1398b21143dca32e4aeec3d383d39bf48d
cituk <programaciondyn3.mx@sandos.com>
2020-07-27 18:32:52 -0500

    Tarjetas con la información del Héroe

--- 07-heroes-app/src/components/heroes/HeroList.js
+++ 07-heroes-app/src/components/heroes/HeroList.js
@@ -1,21 +1,21 @@
 import React from 'react'
 import { getHeroesByPublisher } from '../../selectors/getHeroesByPublisher';
+import { HeroCard } from './HeroCard';
 
 export const HeroList = ({publisher}) => {
 
     const heroes = getHeroesByPublisher(publisher);
 
     return (
-        <ul>
+        <div className="card-columns">
             {
                 heroes.map( hero => (
-                    <li
+                    <HeroCard
                         key={hero.id}
-                    >
-                        {hero.superhero}
-                    </li>
+                        {...hero}
+                    />
                 ))
             }
-        </ul>
+        </div>
     )
 }

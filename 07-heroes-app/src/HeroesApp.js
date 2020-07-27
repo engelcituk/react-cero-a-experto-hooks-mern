6b6a8066755e178a8fc2875faa982317cd56cf19
cituk <programaciondyn3.mx@sandos.com>
2020-07-27 15:20:53 -0500

    Inicio de proyecto - HeroesApp

---
+++ 07-heroes-app/src/HeroesApp.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const HeroesApp = () => {
+    return (
+        <div>
+            <h1>HeroesApp</h1>
+        </div>
+    )
+}
---
+++ 07-heroes-app/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { HeroesApp } from './HeroesApp';
+
+
+ReactDOM.render(
+
+    <HeroesApp />
+  ,
+  document.getElementById('root')
+);

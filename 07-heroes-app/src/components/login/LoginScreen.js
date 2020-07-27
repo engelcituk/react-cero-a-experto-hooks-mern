5c97e7cfadc02f0d3312aa8dbc69f5cfdda59f5a
cituk <programaciondyn3.mx@sandos.com>
2020-07-27 16:20:29 -0500

    History push / replace

--- 07-heroes-app/src/components/login/LoginScreen.js
+++ 07-heroes-app/src/components/login/LoginScreen.js
@@ -1,9 +1,22 @@
 import React from 'react'
 
-export const LoginScreen = () => {
+export const LoginScreen = ({history}) => {
+
+    const handleLogin = () => {
+        //history.push('/');
+        history.replace('/');
+    }
     return (
-        <div>
+        <div className="container mt-5">
             <h1>LoginScreen</h1>
+            <hr/>
+
+            <button
+                className="btn btn-primary"
+                onClick={handleLogin}
+            >
+            Ingresar
+            </button>
         </div>
     )
 }

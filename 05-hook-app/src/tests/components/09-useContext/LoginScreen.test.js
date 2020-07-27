65fc63ff09895eee256844f1ce750a042fd1d85f
cituk <programaciondyn3.mx@sandos.com>
2020-07-27 00:17:52 -0500

    Pruebas de funciones del context

---
+++ 05-hook-app/src/tests/components/09-useContext/LoginScreen.test.js
@@ -0,0 +1,36 @@
+
+import React from 'react';
+import {mount} from 'enzyme';
+import { UserContext } from '../../../components/09-useContext/UserContext';
+import { LoginScreen } from '../../../components/09-useContext/LoginScreen';
+
+
+describe('Pruebas en LoginScreen', () => {
+    const setUser = jest.fn();
+
+    const wrapper = mount(
+        <UserContext.Provider
+            value = {{
+                setUser
+            }}
+        >
+            <LoginScreen/>
+        </UserContext.Provider>
+    )
+
+
+    test('Debe mostrarse correctamente ', () => {
+        expect(wrapper).toMatchSnapshot();        
+    });
+
+    test('Debe de ejecutar el setUser con el a(((rgumento esperado', () => {
+        wrapper.find('button').prop('onClick')();
+
+        expect(setUser).toHaveBeenCalledWith({
+            id:12345,
+            name:'Fercho nando'
+        })
+    });
+    
+     
+});

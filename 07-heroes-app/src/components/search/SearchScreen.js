fb84ce68af574e1823c1800a6b097e18558a2e44
cituk <programaciondyn3.mx@sandos.com>
2020-07-27 20:35:49 -0500

    Aplicar filtro en base al QueryString

--- 07-heroes-app/src/components/search/SearchScreen.js
+++ 07-heroes-app/src/components/search/SearchScreen.js
@@ -1,22 +1,25 @@
-import React from 'react';
-import queryString from 'query-string';
-import { heroes } from '../../data/heroes';
+import React , { useMemo } from 'react';
+import queryString from 'query-string'; //paquete de npm
 import { HeroCard } from '../heroes/HeroCard';
 import { useForm } from '../../hooks/useForm';
 import { useLocation} from 'react-router-dom';
+import { getHeroesByName } from '../../selectors/getHeroesByName';
 
 export const SearchScreen = ({history}) => {
 
     const location  = useLocation();
 
     const { q = '' } = queryString.parse(location.search); //obtengo el query -> q
-    const heroesFiltrados =  heroes;
-
+    
     const [values, handleInputChange]= useForm({
         searchText: q //valor inicial
     });
 
     const {searchText} = values;
+    //memorizo los valores en el query string
+    const heroesFiltrados = useMemo(() => getHeroesByName(q), [q]); //si el query cambia disparo getHeroesByName
+    
+    //const heroesFiltrados =  getHeroesByName(searchText);
 
     const handleSearch = (e) => {
         e.preventDefault();
@@ -53,6 +56,18 @@ export const SearchScreen = ({history}) => {
                 <div className="col-7">
                     <h4>Resultados</h4>
                     <hr/>
+                    { (q === '') &&
+                        <div className="alert alert-info">
+                            Search a Hero
+                        </div>
+                    }
+
+                    { (q !== '' && heroesFiltrados.length === 0) &&
+                        <div className="alert alert-danger">
+                            There is no a hero with { q }
+                        </div>
+                    }
+                    
                     {
                         heroesFiltrados.map( hero => (
                             <HeroCard 

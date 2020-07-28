466e0ee3b904411495853ecafa57ddc2438b8c74
cituk <programaciondyn3.mx@sandos.com>
2020-07-27 19:26:58 -0500

    Animaciones en nuestro componente

--- 07-heroes-app/src/components/heroes/HeroList.js
+++ 07-heroes-app/src/components/heroes/HeroList.js
@@ -9,7 +9,7 @@ export const HeroList = ({publisher}) => {
     const heroes = useMemo(() => getHeroesByPublisher(publisher), [publisher]); //si el publisher cambia disparo la función
 
     return (
-        <div className="card-columns">
+        <div className="card-columns animate__animated animate__fadeIn">
             {
                 heroes.map( hero => (
                     <HeroCard
--- 07-heroes-app/src/components/heroes/HeroScreen.js
+++ 07-heroes-app/src/components/heroes/HeroScreen.js
@@ -30,7 +30,7 @@ export const HeroScreen = ({history}) => {
             <div className="col-4">
                 <img
                     src={`../assets/heroes/${heroeId}.jpg`}
-                    className="img-thumbnail"
+                    className="img-thumbnail animate__animated animate__fadeInLeft"
                     alt={superhero}
                 />
             </div>

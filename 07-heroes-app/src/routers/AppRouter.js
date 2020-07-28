5dcaf7739128a7088b60bf793633d41aad5ea658
cituk <programaciondyn3.mx@sandos.com>
2020-07-28 16:24:15 -0500

    Rutas privadas

--- 07-heroes-app/src/routers/AppRouter.js
+++ 07-heroes-app/src/routers/AppRouter.js
@@ -1,21 +1,30 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
   Route
 } from 'react-router-dom';
 
+import { PrivateRoute } from './PrivateRoute';
 import { LoginScreen } from '../components/login/LoginScreen';
 import { DashboardRoutes } from './DashboardRoutes';
+import { AuthContext } from '../auth/AuthContext';
 
 export const AppRouter = () => {
+    
+    const {user} = useContext(AuthContext); //obtengo los datos del usuario si está logueado
+    
     return (
         <Router>
             <div>
             
                 <Switch>
                     <Route exact path="/login" component={ LoginScreen }/>
-                    <Route path="/" component={ DashboardRoutes }/>
+                    <PrivateRoute
+                        path="/"
+                        component={ DashboardRoutes }
+                        isAuthenticated={user.logged}
+                        />
                 </Switch>
             </div>
         </Router>

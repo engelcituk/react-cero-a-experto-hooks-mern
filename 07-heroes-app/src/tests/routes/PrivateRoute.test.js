3e73349b9b1e60878c62642400d4db07fe7d1810
cituk <programaciondyn3.mx@sandos.com>
2020-07-29 16:12:12 -0500

    Pruebas en el PrivateRoute

---
+++ 07-heroes-app/src/tests/routes/PrivateRoute.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { mount} from 'enzyme';
+import { PrivateRoute } from '../../routers/PrivateRoute';
+import { MemoryRouter } from 'react-router-dom';
+
+describe('Pruebas en PrivateRoute', () => {
+    const props = {
+        location:{
+            pathname: '/marvel'
+        }
+    }
+    test('Debe de mostrar el componente si está autenticado y guardar en localstorage', () => {
+        const wrapper = mount(
+            <MemoryRouter>
+                <PrivateRoute
+                    isAuthenticated={true}
+                    component={ () => <span>Listo!</span>}
+                    {...props}
+                />
+            </MemoryRouter>
+        );
+
+        expect(wrapper.find('span').exists()).toBe(true);
+
+    });
+    
+});

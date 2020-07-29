004eb7c21da29ab1c49d985c9440d2c6ed436ec7
cituk <programaciondyn3.mx@sandos.com>
2020-07-29 16:21:54 -0500

    Probar que el PrivateRoute no muestre el componente

--- 07-heroes-app/src/tests/routes/PrivateRoute.test.js
+++ 07-heroes-app/src/tests/routes/PrivateRoute.test.js
@@ -25,8 +25,24 @@ describe('Pruebas en PrivateRoute', () => {
         );
 
         expect(wrapper.find('span').exists()).toBe(true);
-        expect(localStorage.setItem).toHaveBeenCalledWith('lastPath','/marvel')
+        expect(localStorage.setItem).toHaveBeenCalledWith('lastPath','/marvel');
 
     });
+
+    test('Debe de bloquear el componente si no está autenticado', () => {
+        const wrapper = mount(
+            <MemoryRouter>
+                <PrivateRoute
+                    isAuthenticated={false}
+                    component={ () => <span>Listo!</span>}
+                    {...props}
+                />
+            </MemoryRouter>
+        );
+
+        expect(wrapper.find('span').exists()).toBe(false);
+        expect(localStorage.setItem).toHaveBeenCalledWith('lastPath','/marvel'); 
+    });
+    
     
 });

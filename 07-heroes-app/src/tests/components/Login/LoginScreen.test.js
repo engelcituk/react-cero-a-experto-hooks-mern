c50d9dbee4006cf7ecc3be6e78a73cef493f71a1
cituk <programaciondyn3.mx@sandos.com>
2020-07-31 16:39:01 -0500

    Pruebas en el componente LoginScreen

---
+++ 07-heroes-app/src/tests/components/Login/LoginScreen.test.js
@@ -0,0 +1,57 @@
+import React  from 'react';
+import {mount}  from 'enzyme';
+import { MemoryRouter , Route} from 'react-router-dom';
+import { HeroScreen } from '../../../components/heroes/HeroScreen';
+import { LoginScreen } from '../../../components/login/LoginScreen';
+import { AuthContext } from '../../../auth/AuthContext';
+import { types } from '../../../types/types';
+
+
+describe('Pruebas en LoginScreen', () => {
+
+    const history = {
+        replace: jest.fn(),
+    }
+
+    const contextValue = {
+        dispatch: jest.fn(),
+        user: {
+            logged:false,
+        }
+    };
+
+    const wrapper = mount(
+        <AuthContext.Provider value = {contextValue}>
+
+            <LoginScreen history={history} />
+
+        </AuthContext.Provider>
+    ) 
+
+    test('Debe de mostrarse correctamente ', () => {
+        expect(wrapper).toMatchSnapshot();        
+        
+    });
+
+    test('Debe de realizar el dispatch y la navegación', () => {
+
+        const handleClick = wrapper.find('button').prop('onClick');
+
+        handleClick();
+
+        expect( contextValue.dispatch).toHaveBeenCalledWith({
+            type:  types.login,
+            payload: {
+                name: 'Luisito'
+            }
+        });  
+
+        expect(history.replace).toHaveBeenCalledWith('/');
+
+        localStorage.setItem('lastPath','/dc')
+        handleClick();        
+        expect(history.replace).toHaveBeenCalledWith('/dc');
+    });
+
+    
+});

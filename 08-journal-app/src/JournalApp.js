7df8c85489600e64a53bff11d2f96b818df3d901
cituk <programaciondyn3.mx@sandos.com>
2020-08-05 15:34:18 -0500

    Inicio del proyecto - JournalApp

---
+++ 08-journal-app/src/JournalApp.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const JournalApp = () => {
+    return (
+        <div>
+            <h1>Journal App</h1>
+        </div>
+    )
+}
---
+++ 08-journal-app/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { JournalApp } from './JournalApp';
+
+import './styles/styles.scss'
+
+ReactDOM.render(
+  
+    <JournalApp />
+,
+  document.getElementById('root')
+);
+
+

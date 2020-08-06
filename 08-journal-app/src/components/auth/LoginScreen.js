5165b08fa7ae34f7356b7189ac5b527fe15a91f9
cituk <programaciondyn3.mx@sandos.com>
2020-08-06 15:41:33 -0500

    Estilos del AuthRouter

--- 08-journal-app/src/components/auth/LoginScreen.js
+++ 08-journal-app/src/components/auth/LoginScreen.js
@@ -2,8 +2,31 @@ import React from 'react'
 
 export const LoginScreen = () => {
     return (
-        <div>
-            <h1> Login</h1>
-        </div>
+        <>
+            <h3> Login</h3>
+
+            <form>
+                <input
+                    type="text"
+                    placeholder="email"
+                    name="email"
+                />
+
+                <input
+                    type="password"
+                    placeholder="Password"
+                    name="password"
+                />
+
+                <button
+                    type="submit"
+
+                >
+                    Ingresar
+                </button>
+                <hr/>
+                Google
+            </form>
+        </>
     )
 }
--- 08-journal-app/src/components/routers/AuthRouter.js
+++ 08-journal-app/src/components/routers/AuthRouter.js
@@ -9,21 +9,23 @@ import { RegisterScreen } from '../auth/RegisterScreen';
 
 export const AuthRouter = () => {
     return (
-        <div>
-            <Switch>
-                <Route
-                    exact
-                    path="/auth/login"
-                    component={LoginScreen}
-                />
-                
-                <Route
-                    exact
-                    path="/auth/register"
-                    component={RegisterScreen}
-                />
-                <Redirect to="/auth/login"/>
-            </Switch>
+        <div className="auth__main">
+            <div className="auth__box-container">
+                <Switch>
+                    <Route
+                        exact
+                        path="/auth/login"
+                        component={LoginScreen} 
+                    />
+                    
+                    <Route
+                        exact
+                        path="/auth/register"
+                        component={RegisterScreen}
+                    />
+                    <Redirect to="/auth/login"/>
+                </Switch>
+            </div>
         </div>
     )
 }

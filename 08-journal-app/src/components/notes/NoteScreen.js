9d8bdeab0721df4321550d34aea233b3990f66bb
cituk <programaciondyn3.mx@sandos.com>
2020-08-07 16:25:06 -0500

    Estructura y diseño del HomeScreen

--- 08-journal-app/src/components/notes/NoteScreen.js
+++ 08-journal-app/src/components/notes/NoteScreen.js
@@ -4,7 +4,32 @@ import { NotesAppBar } from './NotesAppBar';
 export const NoteScreen = () => {
     return (
         <div className="notes__main-content">
+           
             <NotesAppBar/>
+            
+            <div className="notes__content">
+                <input
+                    type="text"
+                    placeholder="Some awesome title"
+                    className="notes__title-input"
+                    autoComplete="off"
+                />
+
+                <textarea
+                    placeholder="What happened today?"
+                    className="notes__textarea"
+
+                >
+                </textarea>
+
+                <div className="notes__image">
+                    <img
+                        src="https://s2.best-wallpaper.net/wallpaper/2880x1800/1903/Beautiful-sunrise-purple-sky-moon-river-creative-picture_2880x1800.jpg"
+                        alt="imagen"
+                    />
+                </div>
+            </div>
+
         </div>
     )
 }

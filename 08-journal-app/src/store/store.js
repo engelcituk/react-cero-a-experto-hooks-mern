1901debfa4953011af50d7a86e98d7722fbd7822
cituk <programaciondyn3.mx@sandos.com>
2020-08-27 22:37:00 -0500

    Redux DevTools

--- 08-journal-app/src/store/store.js
+++ 08-journal-app/src/store/store.js
@@ -5,5 +5,8 @@ const reducers = combineReducers({
     auth : authReducer
 });
 
-export const store = createStore(reducers);
+export const store = createStore(
+    reducers,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    );
 

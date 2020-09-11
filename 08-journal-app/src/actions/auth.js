bd79b734b59fb868934a5e226f7df6588f035878
cituk <programaciondyn3.mx@sandos.com>
2020-09-11 00:55:38 -0500

    Logout de Firebase

--- 08-journal-app/src/actions/auth.js
+++ 08-journal-app/src/actions/auth.js
@@ -54,5 +54,13 @@ export const login = (uid, displayName) => ({
     }
 })
    
-       
- 
+export const startLogout = () => {
+    return async (dispatch) => {
+        await firebase.auth().signOut();
+        dispatch(logout());
+    }
+}
+
+export const logout = () => ({
+    type: types.logout
+})
--- 08-journal-app/src/components/journal/Sidebar.js
+++ 08-journal-app/src/components/journal/Sidebar.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import { JournalEntries } from './JournalEntries';
+import {useDispatch} from 'react-redux';
+import { startLogout } from '../../actions/auth';
 
 export const Sidebar = () => {
+
+    const dispatch = useDispatch();
+
+    const handleLogout = () => {
+        dispatch( startLogout() )
+    }
     return (
         <aside className="journal__sidebar">
             <div className="journal__sidebar-navbar">
@@ -10,7 +18,9 @@ export const Sidebar = () => {
                     <span> Jonh Doe</span>
                 </h3>
 
-                <button className="btn">
+                <button className="btn" 
+                    onClick={handleLogout}
+                >
                     Logout
                 </button>
             </div>

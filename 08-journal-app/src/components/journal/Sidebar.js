35da2b551420374ebe625702cf5a530dec5062f4
cituk <programaciondyn3.mx@sandos.com>
2020-09-11 15:31:26 -0500

    Continuación del proyecto - JournalApp

--- 08-journal-app/src/components/journal/Sidebar.js
+++ 08-journal-app/src/components/journal/Sidebar.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { JournalEntries } from './JournalEntries';
-import {useDispatch} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import { startLogout } from '../../actions/auth';
 
 export const Sidebar = () => {
 
     const dispatch = useDispatch();
+    const {name} = useSelector(state => state.auth)
 
     const handleLogout = () => {
         dispatch( startLogout() )
@@ -15,7 +16,7 @@ export const Sidebar = () => {
             <div className="journal__sidebar-navbar">
                 <h3 className="mt-5">
                     <i className="far fa-moon"></i>
-                    <span> Jonh Doe</span>
+                    <span> {name}</span>
                 </h3>
 
                 <button className="btn" 

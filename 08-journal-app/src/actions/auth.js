a80a0ff27bd6fb202193fadc88dded22a80f81b9
cituk <programaciondyn3.mx@sandos.com>
2020-09-10 23:35:49 -0500

    Crear usuario con correo y contraseña

--- 08-journal-app/src/actions/auth.js
+++ 08-journal-app/src/actions/auth.js
@@ -9,6 +9,21 @@ export const startLoginEmailPassword = (email, password) => {
     }
 }
 
+export const startRegisterWithEmailPasswordName = (email, password, name) => {
+    return (dispatch) => {
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+            .then( async ({user}) => {
+               await  user.updateProfile({displayName:name});
+                dispatch(
+                    login(user.uid,user.displayName)
+                )
+            })
+            .catch( e => {
+                console.log(e);
+            })
+    }
+}
+
 export const startGoogleLogin = () => {
     return (dispatch) => {
         firebase.auth().signInWithPopup( googleAuthProvider )
--- 08-journal-app/src/components/auth/RegisterScreen.js
+++ 08-journal-app/src/components/auth/RegisterScreen.js
@@ -5,6 +5,7 @@ import validator from 'validator';
 
 import { useForm } from '../../hooks/useForms';
 import { setError, removeError } from '../../actions/ui';
+import { startRegisterWithEmailPasswordName } from '../../actions/auth';
 
 
 export const RegisterScreen = () => {
@@ -24,7 +25,7 @@ export const RegisterScreen = () => {
     const handleRegister = (e) => {
         e.preventDefault();
         if( isFormValid()){
-            console.log('formulario valido');  
+            dispatch( startRegisterWithEmailPasswordName(email, password, name));
         }
     }
 

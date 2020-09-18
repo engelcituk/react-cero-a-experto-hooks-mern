19fc07f958237d2153f713639d54ea5ba7ff1c56
cituk <programaciondyn3.mx@sandos.com>
2020-09-18 15:54:05 -0500

    Continuación del proyecto - JournalApp - Testing

---
+++ 08-journal-app/src/tests/types/types.test.js
@@ -0,0 +1,26 @@
+import { types } from "../../types/types";
+
+describe('Pruebas con nuestros tipos', () => {
+    
+    test('Debe de tener estos types', () => {
+        expect( types).toEqual({
+            login: '[Auth] Login',
+            logout: '[Auth] Logout',
+
+            uiSetError: '[UI] Set Error',
+            uiRemoveError: '[UI] Remove Error',
+
+            uiStartLoading: '[UI] Start loading',
+            uiFinishLoading: '[UI] Finish loading',
+
+            notesAddNew: '[Notes] New note',
+            notesActive: '[Notes] Set active note',
+            notesLoad: '[Notes] Load notes',
+            notesUpdated: '[Notes] Updated note',
+            notesFileUrl: '[Notes] Updated image url',
+            notesDelete: '[Notes] Delete note',
+            notesLogoutCleaning: '[Notes] Logout cleaning'
+        })
+    })
+    
+})
--- 08-journal-app/src/types/types.js
+++ 08-journal-app/src/types/types.js
@@ -1,24 +1,18 @@
-export const types = {
-    login: '[Auth] Login',
-    logout: '[Auth] Logout',
+    export const types = {
+        login: '[Auth] Login',
+        logout: '[Auth] Logout',
 
-    uiSetError: '[UI] Set Error',
-    uiRemoveError: '[UI] Remove Error',
+        uiSetError: '[UI] Set Error',
+        uiRemoveError: '[UI] Remove Error',
 
-    uiStartLoading: '[UI] Start loading',
-    uiFinishLoading: '[UI] Finish loading',
+        uiStartLoading: '[UI] Start loading',
+        uiFinishLoading: '[UI] Finish loading',
 
-    notesAddNew: '[Notes] New note',
-    notesActive: '[Notes] Set active note',
-    notesLoad: '[Notes] Load notes',
-    notesUpdated: '[Notes] Updated note',
-    notesFileUrl: '[Notes] Updated image url',
-    notesDelete: '[Notes] Delete note',
-    notesLogoutCleaning: '[Notes] Logout cleaning',
-
-
-
-
-
-
-}
+        notesAddNew: '[Notes] New note',
+        notesActive: '[Notes] Set active note',
+        notesLoad: '[Notes] Load notes',
+        notesUpdated: '[Notes] Updated note',
+        notesFileUrl: '[Notes] Updated image url',
+        notesDelete: '[Notes] Delete note',
+        notesLogoutCleaning: '[Notes] Logout cleaning'
+    }

672833cff30e6c9bcf302c2e1fe2603de589ede0
cituk <programaciondyn3.mx@sandos.com>
2020-09-27 15:10:57 -0500

    Crear base de datos de desarrollo

--- 08-journal-app/src/firebase/firebase-config.js
+++ 08-journal-app/src/firebase/firebase-config.js
@@ -11,8 +11,24 @@ const firebaseConfig = {
     messagingSenderId: "453932240479",
     appId: "1:453932240479:web:97b2a55d996ae2129795ea"
   };
+
+  const firebaseConfigTesting = {
+    apiKey: "AIzaSyCe1EBq297tO5ibt63c2GKTehqrmzbhJOM",
+    authDomain: "login-app-88906.firebaseapp.com",
+    databaseURL: "https://login-app-88906.firebaseio.com",
+    projectId: "login-app-88906",
+    storageBucket: "login-app-88906.appspot.com",
+    messagingSenderId: "369450352352",
+    appId: "1:369450352352:web:1b5dd09d382655b9be5a85"
+  };
+
+if (process.env.NODE_ENV === 'test') {
+  firebase.initializeApp(firebaseConfigTesting);  
+} else {
+  //dev/prod
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
+}
 
   const db =  firebase.firestore();
   const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
--- 08-journal-app/src/tests/actions/notes.test.js
+++ 08-journal-app/src/tests/actions/notes.test.js
@@ -1,6 +1,8 @@
 import configureStore from 'redux-mock-store' //ES6 modules
 import thunk from 'redux-thunk'; 
 import { starNewNote } from '../../actions/notes';
+import { types } from '../../types/types';
+import { db } from '../../firebase/firebase-config';
 
 const middlewares = [thunk];
 
@@ -17,6 +19,34 @@ describe('Pruebas con las acciones de notes', () => {
 
     test('Debe de crear una nueva nota starNewNote', async () => {
         await store.dispatch( starNewNote() );
+
+        const actions = store.getActions();
+        
+        expect( actions[0] ).toEqual({
+            type: types.notesActive,
+            payload: {
+                id: expect.any(String),
+                title: '',
+                body: '',
+                date: expect.any(Number)
+              }
+        });
+
+        expect( actions[1] ).toEqual({
+            type: types.notesAddNew,
+            payload: {
+                id: expect.any(String),
+                title: '',
+                body: '',
+                date: expect.any(Number)
+              }
+        });
+
+        const docId = actions[0].payload.id;
+
+        await db.doc(`/testing/journal/notes/${docId}`).delete();
+
     })
+
      
 });

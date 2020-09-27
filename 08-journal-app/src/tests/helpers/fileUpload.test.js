fabeb0bce5c32a7259eef893cf7d24e274dc0255
cituk <programaciondyn3.mx@sandos.com>
2020-09-27 13:08:32 -0500

    Cloudinary SDK - Delete image

--- 08-journal-app/src/tests/helpers/fileUpload.test.js
+++ 08-journal-app/src/tests/helpers/fileUpload.test.js
@@ -1,8 +1,16 @@
+import cloudinary from 'cloudinary';
 import { fileUPload } from '../../helpers/fileUpload';
+
+cloudinary.config({ 
+    cloud_name: 'di3qzyeke', 
+    api_key: '786616524812658', 
+    api_secret: 'Ow80aa6Bv0VsjGgbsuHfEE1VVYQ' 
+  });
+
 
 describe('Pruebas en el fileUpload',  () => {
     
-    test('Debe de cargar un archivo y retorar el URL', async () => {
+    test('Debe de cargar un archivo y retorar el URL', async (done) => {
 
         const resp = await fetch('https://img.pngio.com/una-nube-blanco-stroke-material-imagen-png-para-descarga-gratuita-nube-png-650_410.png');
         const blob = await resp.blob();
@@ -12,6 +20,14 @@ describe('Pruebas en el fileUpload',  () => {
         const url = await fileUPload(file);
 
         expect( typeof url ).toBe('string');
+
+        //borrar por id de la imagen
+        const segments = url.split('/');
+        const imgId = segments[ segments.length - 1].replace('.png','');
+
+        cloudinary.v2.api.delete_resources(imgId, {}, ()=> {
+            done();
+        });
     })
     
 

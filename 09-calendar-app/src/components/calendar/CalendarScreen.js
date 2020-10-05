a64ac50e7339c4047a9f56864320911f6c9d2980
cituk <programaciondyn3.mx@sandos.com>
2020-10-04 21:15:56 -0500

    Configuraciones adicionales al calendario

--- 09-calendar-app/src/components/calendar/CalendarScreen.js
+++ 09-calendar-app/src/components/calendar/CalendarScreen.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import { Navbar } from '../ui/Navbar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import {Calendar, momentLocalizer} from 'react-big-calendar';
 import moment from 'moment';
+import  'moment/locale/es';
+
+import { Navbar } from '../ui/Navbar';
+import { messages } from '../../helpers/calendar-messages-es';
+
+moment.locale('es') //cambio el idioma de moment a español
 
 const localizer = momentLocalizer(moment) // or globalizeLocalizer
 
@@ -14,6 +19,21 @@ const events = [{
 }]
 
 export const CalendarScreen = () => {
+
+    const eventStyleGetter = (event, start, end, isSelected) => {
+
+        const style = {
+            backgroundColor: '#367cf7',
+            borderRadius: '0px',
+            opacy: 0.8,
+            display: 'block',
+            color: 'white'
+        }
+        return {
+            style
+        }
+    };
+
     return (
         <div className="calendar-screen">
             <Navbar/>  
@@ -22,6 +42,8 @@ export const CalendarScreen = () => {
                 events={events}
                 startAccessor="start"
                 endAccessor="end"
+                messages={messages}
+                eventPropGetter={eventStyleGetter}
             />
         </div>
     )

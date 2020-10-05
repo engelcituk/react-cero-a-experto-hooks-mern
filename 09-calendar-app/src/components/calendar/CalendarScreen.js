ebb6949d092148b0a3d020e9dc7c73f81b56e617
cituk <programaciondyn3.mx@sandos.com>
2020-10-04 20:59:28 -0500

    React Big Calendar

--- 09-calendar-app/src/components/calendar/CalendarScreen.js
+++ 09-calendar-app/src/components/calendar/CalendarScreen.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import { Navbar } from '../ui/Navbar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import {Calendar, momentLocalizer} from 'react-big-calendar';
+import moment from 'moment';
+
+const localizer = momentLocalizer(moment) // or globalizeLocalizer
+
+const events = [{
+    title: 'Cumpleaños de Lu',
+    start: moment().toDate(),
+    end: moment().add(2, 'hours').toDate(),
+    bgColor: '#fafafa'
+}]
 
 export const CalendarScreen = () => {
     return (
-        <div>
+        <div className="calendar-screen">
             <Navbar/>  
+            <Calendar
+                localizer={localizer}
+                events={events}
+                startAccessor="start"
+                endAccessor="end"
+            />
         </div>
     )
 }
--- 09-calendar-app/src/index.js
+++ 09-calendar-app/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { CalendarApp } from './CalendarApp';
 
+import './style.css';
 
 ReactDOM.render(
     <CalendarApp />,

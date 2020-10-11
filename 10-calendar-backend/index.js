67f041f3031c3369a18b8184d28554e660b22794
cituk <programaciondyn3.mx@sandos.com>
2020-10-11 16:30:25 -0500

    Configurando Express

--- 10-calendar-backend/index.js
+++ 10-calendar-backend/index.js
@@ -1,1 +1,19 @@
-console.log('hola mundo');
+const express = require('express')
+
+//crear el servidor de express
+const app = express();
+
+//rutas
+app.get('/', (req, res) => {
+
+    console.log('peticion desde / ')
+    res.json({
+        ok: true,
+    });
+});
+
+//escuchar peticiones
+app.listen(4000, () => {
+    console.log(`Servidor corriendo el puerto ${4000}`)
+});
+

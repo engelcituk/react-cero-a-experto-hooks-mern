08b14eec369d41f7878252582144bfc3a8b3502f
cituk <programaciondyn3.mx@sandos.com>
2020-10-14 22:22:47 -0500

    Validar campos necesarios

--- 10-calendar-backend/routes/events.js
+++ 10-calendar-backend/routes/events.js
@@ -3,17 +3,30 @@
  host + /api/eventos
 */
 const { Router} = require('express'); //desestructuro de express y obtengo a Router
-const router = Router();
-const { validarJwt } =require('../middlewares/validarJwt')
+const { check } = require('express-validator'); //middleware para validar campos
 
+const { validarCampos } = require('../middlewares/validarCampos') //middleware para verificar sino hay errores en los campos
+const { validarJwt } =require('../middlewares/validarJwt')
+const { isDate } =require('../helpers/isDate')
 const { getEventos, crearEvento, actualizarEvento, eliminarEvento } = require('../controllers/events');
+
+
+const router = Router();
 
 //como todas las peticiones a esta ruta, podemos resumir la aplicación del midleware de la sig manera:
 router.use(validarJwt);
 
 router.get('/',  getEventos ); 
 
-router.post('/',  crearEvento ); 
+router.post(
+    '/',
+    [
+        check('title','El título es obligatorio').not().isEmpty(),
+        check('start','Fecha de inicio es obligatorio').custom( isDate ),
+        validarCampos
+    ],
+    crearEvento
+); 
 
 router.put('/:id',  actualizarEvento ); 
 

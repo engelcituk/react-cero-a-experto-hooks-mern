90893fb6a23d487ea639cae9f9b4c724ea86197e
cituk <programaciondyn3.mx@sandos.com>
2020-10-14 23:30:00 -0500

    Actualizar un Evento

--- 10-calendar-backend/controllers/events.js
+++ 10-calendar-backend/controllers/events.js
@@ -44,12 +44,57 @@ const crearEvento = async (req, res = response ) => {
 
 }
 
-const actualizarEvento =  (req, res = response ) => {
+const actualizarEvento =  async ( req, res = response ) => {
     
-    res.status(200).json({
-        ok: true,
-        msg: 'actualizar evento'
-    });
+    const idEvento = req.params.id;
+    const uid = req.uid;
+
+
+    try {
+
+        const evento = await Evento.findById(idEvento);
+
+        //si evento no existe
+        if( !evento ){
+
+            res.status(404).json({
+                ok: false,
+                msg: 'El evento no existe en la base de datos'
+            });
+        } 
+        //verifico si la persona que quiere actualizar el evento sea el mismo que lo creó
+        if( evento.user.toString() !== uid){ //en user solo se tiene el id del usuario
+
+            res.status(401).json({
+                ok: false,
+                msg: 'No tiene privilegio para editar este evento'
+            });
+        }
+
+        const nuevoEvento = {
+            ...req.body, //desestruturo todo lo que viene en el body y le agrego el uid para el user
+            user: uid
+        }
+
+        //{new: true} es para indicar que se regrese el evento ya actualizado y no el viejo
+        const eventoActualizado = await Evento.findByIdAndUpdate( idEvento, nuevoEvento,{ new : true } );
+
+        res.status(200).json({
+            ok: true,
+            evento: eventoActualizado
+        });
+        
+    } catch (error) {
+        
+        console.log(error);
+
+        res.status(500).json({
+            ok: false,
+            msg: 'Hable con el administrador'
+        });
+    }
+
+    
 
 }
 

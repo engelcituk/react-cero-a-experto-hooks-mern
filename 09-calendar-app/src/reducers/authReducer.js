390e7fc26355ff7572d1a0932e577b19fdb608a3
cituk <programaciondyn3.mx@sandos.com>
2020-10-18 17:49:15 -0500

    AuthReducer

---
+++ 09-calendar-app/src/reducers/authReducer.js
@@ -0,0 +1,16 @@
+
+const initialState = {
+    checking: true,
+   /*  uid:null,
+    name: null */
+}
+
+export const authReducer = ( state= initialState, action) => {
+
+    switch (action.type) {
+        
+    
+        default:
+            return state;
+    }
+}
--- 09-calendar-app/src/reducers/rootReducer.js
+++ 09-calendar-app/src/reducers/rootReducer.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 
 import { uiReducer } from './uiReducer';
 import { calendarReducer } from './calendarReducer';
+import { authReducer } from './authReducer';
 
  
 export const rootReducer = combineReducers({
     ui: uiReducer,
-    calendar: calendarReducer
+    calendar: calendarReducer,
+    auth: authReducer
 
 })

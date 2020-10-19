c4cf7513331c3aa9a7799d2854b55778d1a9b43a
cituk <programaciondyn3.mx@sandos.com>
2020-10-18 20:04:08 -0500

    Añadir el UID y el Name del usuario en el Backend

--- 10-calendar-backend/controllers/auth.js
+++ 10-calendar-backend/controllers/auth.js
@@ -96,13 +96,13 @@ const revalidarToken = async (req, res = response) => {
     const uid = req.uid;
     const name = req.name;
     // const {uid, name} = req; //mas elegante con desestructuración
-
-
     //generar un nuevo JWT y retornarlo en esta petición
     const token = await generarJWT( uid, name );
 
     res.json({
         ok: true,
+        uid,
+        name,
         token
     });
 }
